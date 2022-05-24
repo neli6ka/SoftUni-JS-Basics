1e2256f3cbaaa00ed7335868ac93ae330e69cdbf
neli6ka <kamenova.nel@gmail.com>
2022-05-24 08:01:46 +0300

    Create isEvenOrOdd.js

---
+++ Conditional Statements - Lab/isEvenOrOdd.js
@@ -0,0 +1,15 @@
+function isEvenOrOdd(input) {
+    let num = Number(input[0]);
+    if (num % 2 == 0) {
+        console.log("even");
+    } else {
+        console.log("odd");
+
+    }
+
+}
+
+// isEvenOrOdd(["2"]);
+// isEvenOrOdd(["3"]);
+// isEvenOrOdd(["25"]);
+// isEvenOrOdd(["1024"]);
