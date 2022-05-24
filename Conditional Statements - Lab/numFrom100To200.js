79daa1b56d28c9ebf4d18f562e0893d0a06b10e3
neli6ka <kamenova.nel@gmail.com>
2022-05-24 08:10:19 +0300

    Create numFrom100To200.js

---
+++ Conditional Statements - Lab/numFrom100To200.js
@@ -0,0 +1,12 @@
+function numFrom100To200(input) {
+    let num = Number(input[0]);
+    if (num < 100) {
+        console.log("Less than 100");
+    } else if(num >= 100 && num <= 200) {
+        console.log("Between 100 and 200");
+    } else {
+        console.log("Greater than 200");
+    }
+} 
+
+//numFrom100To200(["210"]);
