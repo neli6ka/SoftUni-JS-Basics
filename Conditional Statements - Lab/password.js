effbc6d3652cfb97daf121c3eeee78d6a461edb3
neli6ka <kamenova.nel@gmail.com>
2022-05-24 08:08:06 +0300

    Create guessPassword.js

---
+++ Conditional Statements - Lab/password.js
@@ -0,0 +1,10 @@
+function guessPassword(input) {
+    let password = "s3cr3t!P@ssw0rd";
+    if (input == "s3cr3t!P@ssw0rd") {
+        console.log("Welcome");
+    } else {
+        console.log("Wrong password!");
+    }
+}
+
+//guessPassword(["qwerty"]);
