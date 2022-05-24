0f360e088ff30f00e45d5ea916f05ad3992c3ffb
neli6ka <kamenova.nel@gmail.com>
2022-05-24 08:02:29 +0300

    Create greatherNumber.js

---
+++ Conditional Statements - Lab/greatherNumber.js
@@ -0,0 +1,12 @@
+function greatherNumber(input) {
+    let firstNum = Number(input[0]);
+    let secondNum = Number(input[1]);
+
+    if (firstNum > secondNum) {
+        console.log(firstNum);      
+    } else {
+        console.log(secondNum);
+    }
+}
+
+
